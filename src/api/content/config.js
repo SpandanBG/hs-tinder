abd4d828e5bd505660a4a66ab1780130d9226e66
Ayesha Mundu <ayeshamundu@NOVI-NWQQ7WJ562.local>
2023-05-13 20:40:28 +0530

    fixed fast preloading

--- src/api/content/config.js
+++ src/api/content/config.js
@@ -2,216 +2,252 @@
 const config = [
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/mando1.mp4',
+    title: 'mando1',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8443/1478443-t-10b91af9a1a8',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/kwk2.mp4',
+    title: 'kwk2',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/642/1420642-t-44349d7cf5ac',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/mf1.mp4',
+    title: 'mf1',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/mando3.mp4',
+    title: 'mando3',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8443/1478443-t-10b91af9a1a8',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/test-vid2.mp4',
+    title: 'vid2',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_148/sources/r1/cms/prod/6197/1516197-t-d6940a1ba339',
     type: 'AD'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/pk2.mp4',
+    title: 'pk2',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/9407/1489407-t-ec648886d948',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/av4.mp4',
+    title: 'av4',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/6722/1366722-t-48964025ccfe',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/test-vid3.mp4',
+    title: 'vid3',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/2844/1382844-t-c40828be0daf',
     type: 'AD'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/pk3.mp4',
+    title: 'pk3',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/9407/1489407-t-ec648886d948',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/pk4.mp4',
+    title: 'pk4',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/9407/1489407-t-ec648886d948',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/av2.mp4',
+    title: 'av2',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/6722/1366722-t-48964025ccfe',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/mando2.mp4',
+    title: 'mando2',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8443/1478443-t-10b91af9a1a8',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/kwk3.mp4',
+    title: 'kwk3',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/642/1420642-t-44349d7cf5ac',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/test-vid3.mp4',
+    title: 'vid3',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/2844/1382844-t-c40828be0daf',
     type: 'AD'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/pk5.mp4',
+    title: 'pk5',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/9407/1489407-t-ec648886d948',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/av3.mp4',
+    title: 'av3',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/6722/1366722-t-48964025ccfe',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/wif2.mp4',
+    title: 'wif2',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/7434/1417434-t-29d35b27dc41',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/mf4.mp4',
+    title: 'mf4',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/test-vid.mp4',
+    title: 'vid',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/2844/1382844-t-c40828be0daf',
     type: 'AD'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/kwk4.mp4',
+    title: 'kwk4',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/642/1420642-t-44349d7cf5ac',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/mfd2.mp4',
+    title: 'mfd2',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/fob2.mp4',
+    title: 'fob2',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8566/1378566-t-158d0cbd8454',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/mf5.mp4',
+    title: 'mf5',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/fob4.mp4',
+    title: 'fob4',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8566/1378566-t-158d0cbd8454',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/fob1.mp4',
+    title: 'fob1',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8566/1378566-t-158d0cbd8454',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/pk1.mp4',
+    title: 'pk1',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/9407/1489407-t-ec648886d948',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/mfd1.mp4',
+    title: 'mfd1',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/wif1.mp4',
+    title: 'wif1',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/7434/1417434-t-29d35b27dc41',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/av5.mp4',
+    title: 'av5',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/6722/1366722-t-48964025ccfe',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/mf1.mp4',
+    title: 'mf1',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/mfd3.mp4',
+    title: 'mfd3',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/action/av1.mp4',
+    title: 'av1',
     genre: ['ACTION'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/6722/1366722-t-48964025ccfe',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/kwk1.mp4',
+    title: 'kwk1',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/642/1420642-t-44349d7cf5ac',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/drama/kwk5.mp4',
+    title: 'kwk5',
     genre: ['DRAMA'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/642/1420642-t-44349d7cf5ac',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/fob3.mp4',
+    title: 'fob3',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/8566/1378566-t-158d0cbd8454',
     type: 'CLIP'
 },
 {
     src: 'https://secure-media.hotstar.com/static/asmm/comedy/mf3.mp4',
+    title: 'mf3',
     genre: ['COMEDY'],
     contentImg: 'https://img1.hotstarext.com/image/upload/f_auto,h_136/sources/r1/cms/prod/5229/1405229-t-e662c6c4d669',
     type: 'CLIP'
